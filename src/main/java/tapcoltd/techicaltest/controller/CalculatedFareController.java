f5230d89acc310cf8fde42d69a7dd775b0686f27
gjc24 <60393122+gjc24@users.noreply.github.com>
2022-12-06 15:16:11 +0800

    Implemented enable cross-origin

--- src/main/java/tapcoltd/techicaltest/controller/CalculatedFareController.java
+++ src/main/java/tapcoltd/techicaltest/controller/CalculatedFareController.java
@@ -4,10 +4,7 @@ package tapcoltd.techicaltest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tapcoltd.techicaltest.entity.CalculatedFare;
 import tapcoltd.techicaltest.entity.Driver;
 import tapcoltd.techicaltest.service.CalculatedFareService;
@@ -15,7 +12,7 @@ import tapcoltd.techicaltest.service.DriverService;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("api/fare")
 public class CalculatedFareController {
